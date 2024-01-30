dda4304132870adff598a517140bd953e0bea56f
leite-leo <leoinatel@gmail.com>
2024-01-30 16:32:42 -0300

    Controller layer - create update (Customer)

--- credit-application-system/src/main/kotlin/leoleite/creditapplication/system/controller/CustomerController.kt
+++ credit-application-system/src/main/kotlin/leoleite/creditapplication/system/controller/CustomerController.kt
@@ -1,15 +1,18 @@
 package leoleite.creditapplication.system.controller
 
 import leoleite.creditapplication.system.controller.dto.CustomerDto
+import leoleite.creditapplication.system.controller.dto.CustomerUpdateDto
 import leoleite.creditapplication.system.controller.dto.CustomerView
 import leoleite.creditapplication.system.entity.Customer
 import leoleite.creditapplication.system.service.implementation.CustomerService
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -31,4 +34,13 @@ class CustomerController(
 
   @DeleteMapping("/{id}")
   fun deleteCustomer(@PathVariable id: Long) = this.customerService.delete(id)
+
+  @PatchMapping
+  fun updadeCustomer(@RequestParam(value = "customerId") id: Long,
+                     @RequestBody customerUpdateDto: CustomerUpdateDto): CustomerView {
+    val customer: Customer = this.customerService.findById(id)
+    val customerToUpdate: Customer = customerUpdateDto.toEntity(customer)
+    val customerUpdated: Customer = this.customerService.save(customerToUpdate)
+    return CustomerView(customerUpdated)
+  }
 }
