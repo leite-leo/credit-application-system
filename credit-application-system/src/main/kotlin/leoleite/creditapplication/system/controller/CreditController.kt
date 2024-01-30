952a408a39eb03dd06e7dfcca825c5d4164ca6b6
leite-leo <leoinatel@gmail.com>
2024-01-30 18:33:01 -0300

    Controller layer - create findAllCustomerCredits (credits)

--- credit-application-system/src/main/kotlin/leoleite/creditapplication/system/controller/CreditController.kt
+++ credit-application-system/src/main/kotlin/leoleite/creditapplication/system/controller/CreditController.kt
@@ -1,12 +1,16 @@
 package leoleite.creditapplication.system.controller
 
 import leoleite.creditapplication.system.controller.dto.CreditDto
+import leoleite.creditapplication.system.controller.dto.CreditViewList
 import leoleite.creditapplication.system.entity.Credit
 import leoleite.creditapplication.system.service.implementation.CreditService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.stream.Collectors
 
 @RestController
 @RequestMapping("/api/credits")
@@ -17,5 +21,13 @@ class CreditController(
   fun savecredit(@RequestBody creditDto: CreditDto): String {
     val credit: Credit = this.creditService.save(creditDto.toEntity())
     return "Credit ${credit.creditCode} - Customer ${credit.customer?.firstName} registred!"
+  }
+
+  @GetMapping
+  fun findAllByCustomerId(@RequestParam(value ="customerId") customerId: Long): List<CreditViewList> {
+    return this.creditService.findAllByCustomer(customerId)
+      .stream().map { credit: Credit -> CreditViewList(credit) }
+      .collect(Collectors.toList())
+
   }
 }
