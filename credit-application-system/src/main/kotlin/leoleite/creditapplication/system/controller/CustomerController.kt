f02dee2dd5cb17583839fd37f120643263409ea7
leite-leo <leoinatel@gmail.com>
2024-01-30 15:42:49 -0300

    Start Controller layer - create save endpoint.

---
+++ credit-application-system/src/main/kotlin/leoleite/creditapplication/system/controller/CustomerController.kt
@@ -0,0 +1,20 @@
+package leoleite.creditapplication.system.controller
+
+import leoleite.creditapplication.system.controller.dto.CustomerDto
+import leoleite.creditapplication.system.service.implementation.CustomerService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/customers")
+class CustomerController(
+  private val customerService: CustomerService
+) {
+  @PostMapping
+  fun saveCustomer(@RequestBody customerDto: CustomerDto): String {
+    val savedCustomer = this.customerService.save(customerDto.toEntity())
+    return "Customer ${savedCustomer.email} registred!"
+  }
+}
--- credit-application-system/src/main/kotlin/leoleite/creditapplication/system/entity/Customer.kt
+++ credit-application-system/src/main/kotlin/leoleite/creditapplication/system/entity/Customer.kt
@@ -1,6 +1,7 @@
 package leoleite.creditapplication.system.entity
 
 import jakarta.persistence.*
+import java.math.BigDecimal
 
 @Entity
 // @Table(name = "Cliente")
@@ -9,6 +10,7 @@ data class Customer(
     @Column(nullable = false) var firstName: String = "",
     @Column(nullable = false) var lastName: String = "",
     @Column(nullable = false, unique = true) val cpf: String,
+    @Column(nullable = false) var income: BigDecimal = BigDecimal.ZERO,
     @Column(nullable = false, unique = true) var email: String = "",
     @Column(nullable = false) var password: String = "",
     @Column(nullable = false) @Embedded var address: Address = Address(),
