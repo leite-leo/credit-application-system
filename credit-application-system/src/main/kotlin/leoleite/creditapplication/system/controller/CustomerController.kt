acec844dee6a5ad3f15b5edebea16918a666a1b6
leite-leo <leoinatel@gmail.com>
2024-01-30 16:08:49 -0300

    Controller layer - create findById.

--- credit-application-system/src/main/kotlin/leoleite/creditapplication/system/controller/CustomerController.kt
+++ credit-application-system/src/main/kotlin/leoleite/creditapplication/system/controller/CustomerController.kt
@@ -1,7 +1,11 @@
 package leoleite.creditapplication.system.controller
 
 import leoleite.creditapplication.system.controller.dto.CustomerDto
+import leoleite.creditapplication.system.controller.dto.CustomerView
+import leoleite.creditapplication.system.entity.Customer
 import leoleite.creditapplication.system.service.implementation.CustomerService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,5 +20,11 @@ class CustomerController(
   fun saveCustomer(@RequestBody customerDto: CustomerDto): String {
     val savedCustomer = this.customerService.save(customerDto.toEntity())
     return "Customer ${savedCustomer.email} registred!"
+  }
+
+  @GetMapping("/{id}")
+  fun findById(@PathVariable id: Long): CustomerView {
+    val customer: Customer = this.customerService.findById(id)
+    return CustomerView(customer)
   }
 }
